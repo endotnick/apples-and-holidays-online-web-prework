1f092ce03bd49b17af4f328a56a18306f3858eea
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 00:53:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash[k].each do |k, v|
       holiday = k.to_s
       holiday[0] = holiday[0].capitalize
-      supplies = v.to_s
+      supplies = v.join
       puts "  #{holiday}: #{supplies}"
     end
   end
