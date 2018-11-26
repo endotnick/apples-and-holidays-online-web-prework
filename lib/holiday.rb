c0facab00d8154ece66ce670c69261b604278b7d
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 01:10:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,8 +67,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, holidays|
-    holidays.select do |k, v|
+  result = holiday_hash.select do |season, holidays|
+    holidays.each do |v|
       v.include?('BBQ')
     end
   end
